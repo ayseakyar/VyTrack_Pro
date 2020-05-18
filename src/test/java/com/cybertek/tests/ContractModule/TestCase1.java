6b1e915dabe6c6ef048e29a7f096ed79f19cca09
R.Alioglu <43556960+regalioglu21@users.noreply.github.com>
2020-05-18 17:38:55 +0200

    Update TestCase1.java

--- src/test/java/com/cybertek/tests/ContractModule/TestCase1.java
+++ src/test/java/com/cybertek/tests/ContractModule/TestCase1.java
@@ -1,1 +1,9 @@
-package com.cybertek.tests.ContractModule;import com.cybertek.tests.TestBase;public class TestCase1 extends TestBase {}
+package com.cybertek.tests.ContractModule;
+
+import com.cybertek.tests.TestBase;
+
+public class TestCase1 extends TestBase {
+  
+  this part is form github 
+
+}
