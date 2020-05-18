a19df8ed32776e5897915d1303793dcd1e26ec9f
husenjanobul <husenjanobul@gmail.com>
2020-05-18 19:11:53 +0200

    update BasePage.java

--- src/test/java/com/cybertek/pages/BasePage.java
+++ src/test/java/com/cybertek/pages/BasePage.java
@@ -2,6 +2,11 @@ package com.cybertek.pages;
 
 public abstract class BasePage {
 
+    public void goToMyUser(){
+        waitUntilLoaderScreenDisappear();
+        BrowserUtils.waitForClickablility(userName, 5).click();
+        BrowserUtils.waitForClickablility(myUser, 5).click();
+    }
 
 
 }
