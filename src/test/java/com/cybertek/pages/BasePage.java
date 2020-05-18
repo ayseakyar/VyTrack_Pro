6757fe6b96a1c8ef410a506b1b6b6791f97dd4bc
chrisspoll <sevin_b@yahoo.com>
2020-05-18 22:20:58 +0200

    update BasePage.java

--- src/test/java/com/cybertek/pages/BasePage.java
+++ src/test/java/com/cybertek/pages/BasePage.java
@@ -1,6 +1,13 @@
 package com.cybertek.pages;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public abstract class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
 
     public void goToMyUser(){
         waitUntilLoaderScreenDisappear();
