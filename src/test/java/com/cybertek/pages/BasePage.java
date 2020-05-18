4f9bf9697cfe3d3ea2ab7f9e7ea89dd10da94a05
Osman OLGUN <osmanolgun.de@gmail.com>
2020-05-18 19:50:12 +0200

    osman basepag update2

--- src/test/java/com/cybertek/pages/BasePage.java
+++ src/test/java/com/cybertek/pages/BasePage.java
@@ -1,10 +1,19 @@
 package com.cybertek.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
 public abstract class BasePage {
 
     @FindBy(css = "div[class='loader-mask shown']")
     @CacheLookup
     protected WebElement loaderMask;
+
+    @FindBy(css = "h1[class='oro-subtitle']")
+    public WebElement pageSubTitle;
+    @FindBy(css = "#user-menu > a")
+    public WebElement userName;
 
     public void goToMyUser(){
         waitUntilLoaderScreenDisappear();
