d9264fffb91eaf6184aeabf62013e661e7688127
Cihan GÜLER <58997250+cihanguler@users.noreply.github.com>
2020-05-20 19:22:19 +0200

    Update BasePage.java (#12)
    Co-authored-by: elifgc <gencelif3406@gmail.com>

--- src/test/java/com/cybertek/pages/BasePage.java
+++ src/test/java/com/cybertek/pages/BasePage.java
@@ -28,6 +28,17 @@ public abstract class BasePage {
         BrowserUtils.waitForClickablility(userName, 5).click();
         BrowserUtils.waitForClickablility(myUser, 5).click();
     }
+
+    public void navigateToModule(String tab, String module) {
+        String tabLocator = "//span[normalize-space()='" + tab + "' and contains(@class, 'title title-level-1')]";
+        String moduleLocator = "//span[normalize-space()='" + module + "' and contains(@class, 'title title-level-2')]";
+        try {
+            BrowserUtils.waitForClickablility(By.xpath(tabLocator), 5);
+            WebElement tabElement = Driver.get().findElement(By.xpath(tabLocator));
+            new Actions(Driver.get()).moveToElement(tabElement).pause(200).doubleClick(tabElement).build().perform();
+        } catch (Exception e) {
+            BrowserUtils.clickWithWait(By.xpath(tabLocator), 5);
+        }
   
       public BasePage() {
         PageFactory.initElements(Driver.get(), this);
